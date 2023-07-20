8d1fe9b08fd0cbbb58e326c6ff08713e051569cf
Md-FarhadHossain <freelancerfarhadhossain@gmail.com>
2023-07-20 13:59:51 +0600

    dashboard title and description added

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,7 +1,17 @@
 export default function DashboardPage() {
   return (
     <div>
-      <p>Dashboard (Protected)</p>
+     
+      <div className="space-y-4 mb-8">
+        {/* Title */}
+        <h2 className="text-2xl md:text-4xl font-bold text-center">
+          Explore the power of AI
+        </h2>
+        {/* Description */}
+        <p className="text-muted-foreground font-light text-sm md:text-lg text-center">
+          Chat with the Smartest AI - Experience the power of AI
+        </p>
+      </div>
       
     </div>
   )
