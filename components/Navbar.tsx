3c36852352f95aa3db0c991e1a8d155b5888bb86
Md-FarhadHossain <freelancerfarhadhossain@gmail.com>
2023-07-20 13:24:15 +0600

    the sheet or sidebar install by shadcn

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,21 +1,18 @@
-import React from 'react'
-import { Button } from './ui/button'
-import { Menu } from 'lucide-react'
-import { UserButton } from '@clerk/nextjs'
+import React from "react";
+import { Button } from "./ui/button";
+import { Menu } from "lucide-react";
+import { UserButton } from "@clerk/nextjs";
+import MobileSidebar from "./mobile-sidebar";
 
 const Navbar = () => {
   return (
-    <div className='flex items-center p-4'>
-        <Button variant='ghost' size='icon' className='md:hidden'>
-            <Menu />
-
-        </Button>
-
-        <div className='flex w-full justify-end'>
-            <UserButton afterSignOutUrl='/' />
-        </div>
+    <div className="flex items-center p-4">
+      <MobileSidebar />
+      <div className="flex w-full justify-end">
+        <UserButton afterSignOutUrl="/" />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
---
+++ components/mobile-sidebar.tsx
@@ -0,0 +1,14 @@
+"use client";
+import React from "react";
+import { Button } from "./ui/button";
+import { Menu } from "lucide-react";
+
+const MobileSidebar = () => {
+  return (
+    <Button variant="ghost" size="icon" className="md:hidden">
+      <Menu />
+    </Button>
+  );
+};
+
+export default MobileSidebar;
