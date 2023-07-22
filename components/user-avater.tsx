5eba021ec1a9ebb1de61e7de767ef10edce0a403
Md-FarhadHossain <freelancerfarhadhossain@gmail.com>
2023-07-23 01:21:15 +0600

    user avater has created with shadcn with picture and without picture also

--- components/user-avater.tsx
+++ components/user-avater.tsx
@@ -1,7 +1,16 @@
+import { useUser } from '@clerk/nextjs'
 import React from 'react'
+import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar'
 
 export const UserAvater = () => {
+    const { user } = useUser()
   return (
-    <div>UserAvater</div>
+    <Avatar className='w-8 h-8'>
+        <AvatarImage src={user?.profileImageUrl} />
+        <AvatarFallback>
+            {user?.firstName?.charAt(0)}
+            {user?.lastName?.charAt(0)}
+        </AvatarFallback>
+    </Avatar>
   )
 }
