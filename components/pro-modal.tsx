11e064733c13f5dfa4f27b5c9d5bd63611203dce
md-FarhadHossain <freelancerfarhadhossain@gmail.com>
2023-08-09 01:05:52 -0700

    everythis is now updto date

--- components/pro-modal.tsx
+++ components/pro-modal.tsx
@@ -1,9 +1,23 @@
 "use client"
 
 import React from 'react'
+import { Dialog, DialogContent, DialogHeader } from '@/components/ui/dialog'
+import { DialogTitle } from '@radix-ui/react-dialog'
+import { useProModal } from '@/hooks/use-pro-modal'
 
 export const ProModal  = () => {
+    
+    const proModal = useProModal();
+
   return (
-    <div>ProModal </div>
+    <Dialog open={proModal.isOpen}>
+        <DialogContent>
+            <DialogHeader>
+                <DialogTitle className='flex justify-center items-center flex-col gap-y-4 pb-2'>
+                    Upgrade to Genius
+                </DialogTitle>
+            </DialogHeader>
+        </DialogContent>
+    </Dialog>
   )
 }
