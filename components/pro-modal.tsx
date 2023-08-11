7a474831815ec74bb1ac385a2e6e047bc000257c
md-FarhadHossain <freelancerfarhadhossain@gmail.com>
2023-08-11 15:53:38 +0600

    the modal badge added with pro

--- components/pro-modal.tsx
+++ components/pro-modal.tsx
@@ -4,17 +4,23 @@ import React from 'react'
 import { Dialog, DialogContent, DialogHeader } from '@/components/ui/dialog'
 import { DialogTitle } from '@radix-ui/react-dialog'
 import { useProModal } from '@/hooks/use-pro-modal'
+import { Badge } from './ui/badge'
 
 export const ProModal  = () => {
     
     const proModal = useProModal();
 
   return (
-    <Dialog open={proModal.isOpen}>
+    <Dialog open={proModal.isOpen} onOpenChange={proModal.onClose}>
         <DialogContent>
             <DialogHeader>
                 <DialogTitle className='flex justify-center items-center flex-col gap-y-4 pb-2'>
-                    Upgrade to Genius
+                    <div className='flex items-center gap-x-2 font-bold py-1'>
+                        Upgrade to Genius
+                        <Badge className='uppercase py-1 text-sm'>
+                            Pro
+                        </Badge>
+                    </div>
                 </DialogTitle>
             </DialogHeader>
         </DialogContent>
